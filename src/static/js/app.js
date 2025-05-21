1549471139490cdeb19faa95fe9867330e3a5a8b
DHarrison <dharrison5@ewu.edu>
2025-05-20 21:04:00 -0700

    first commit

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">ADD AN ITEM DEVIN THE LIST IS EMPTY</p>
             )}
             {items.map(item => (
                 <ItemDisplay
